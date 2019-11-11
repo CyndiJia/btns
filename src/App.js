4690af49f4498a92e524f7b62610e01aa62d3b7f
Cyndi <31037969+CyndiJia@users.noreply.github.com>
2019-11-11 11:41:30 +0800

    color and interactions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+let btns = [
+  {
+    id: "b1",
+    name: "First Button"
+  },
+  {
+    id:"b2",
+    name: "Second Button"
+  },
+  {
+    id:"b3",
+    name: "Third Button"
+  }
+]
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Whole data={btns}/>
     </div>
   );
+}
+
+function Whole({data}){
+  const[select,changeSelection] = useState('');
+  return(
+    <div className="main">
+      <div className="con">
+        {data.map(d=>{return <Button name = {d.name} id = {d.id} onchange={changeSelection} selected={select}/>})}
+      </div>
+      <div className="blank">
+        <p id="text">You have selected : {select}</p>
+      </div>
+    </div>
+   
+  );
+}
+
+function Button({name,id,selected,onchange}){
+  const[nofill,changeFill] = useState(false);
+  
+  function changeContent(){
+    changeFill(true);
+    onchange(name);
+    setTimeout(back,5000);
+ }
+  function back(){
+    changeFill(false);
+ }
+ console.log({selected});
+    return(
+    <div className="three">
+      <div className = {'b'+" "+(nofill?'fill':'')} id={id} onClick={changeContent}>
+        <p id ={nofill?'textclr':''}>{name}</p>
+      </div>
+    </div>
+ 
+    );
+    
 }
 
 export default App;
